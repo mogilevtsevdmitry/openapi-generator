98700064aac41735db334a0c829b095ead1ded1b
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2025-03-16 00:43:06 +0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
       // @ts-ignore
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/openapi-generator/',
+//   base: '/openapi-generator/',
+  publicDir: 'public',
 });
